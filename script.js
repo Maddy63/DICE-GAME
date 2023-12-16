d543be82f89f4d22a6c65f012441729913811cb0
Maddy63 <128783490+Maddy63@users.noreply.github.com>
2023-12-16 14:44:01 +0530

    imagelink

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ let btnNewGame = document.querySelector(".new-game");
 //initial initialisation
 let currentScore = 0;
 let tCurrntScore = 0;
-diceImage.src = "/images/start.PNG";
+diceImage.src = "images/start.PNG";
 playerOne.classList.add("play");
 playerTwo.classList.add("notPlay");
 //dice roll button to change the dice image
 const diceFaceChange = function () {
   if (tCurrntScore <= 100) {
     const diceNumber = Math.trunc(Math.random() * 6) + 1;
-    diceImage.src = `/images/dice-${diceNumber}.PNG`;
+    diceImage.src = `images/dice-${diceNumber}.PNG`;
     if (diceNumber === 1) {
       playerChange();
     } else {
@@ -66,7 +66,7 @@ const resetGame = function () {
   document.querySelector(".p1Score").textContent = 0;
   document.querySelector(".p2Score").textContent = 0;
   pScore = document.querySelector(".p1Score");
-  diceImage.src = "/images/start.PNG";
+  diceImage.src = "images/start.PNG";
 };
 rollDice.addEventListener("click", diceFaceChange);
 holdScore.addEventListener("click", holdPresentScore);
